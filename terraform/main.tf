7b756a2b246baebf7cda0db00841e57400e588ef
minuchi <kimkimee2@gmail.com>
2023-08-08 00:25:41 +0900

    feat: add aws api gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "lambda_function" {
-  source = "terraform-aws-modules/lambda/aws"
+  source  = "terraform-aws-modules/lambda/aws"
   version = "5.3.0"
 
   function_name = var.lambda_function_name
@@ -11,10 +11,36 @@ module "lambda_function" {
   handler       = "bootstrap"
   runtime       = "go1.x"
 
-  create_package  = false
+  create_package         = false
   local_existing_package = "this_is_for_terraform_apply.zip"
 
   tags = {
     Name = var.lambda_function_name
+  }
+}
+
+module "api_gateway" {
+  source = "terraform-aws-modules/apigateway-v2/aws"
+
+  name          = var.api_gateway_name
+  description   = var.api_gateway_description
+  protocol_type = "HTTP"
+
+  cors_configuration = {
+    allow_headers = ["content-type", "x-amz-date", "authorization", "x-api-key", "x-amz-security-token", "x-amz-user-agent"]
+    allow_methods = ["*"]
+    allow_origins = ["*"]
+  }
+
+  integrations = {
+    "$default" = {
+      lambda_arn = module.lambda_function.lambda_function_arn
+    }
+  }
+
+  create_api_domain_name = false
+
+  tags = {
+    Name = var.api_gateway_name
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,13 @@ variable "lambda_function_description" {
   description = "Lambda function description"
   default     = "A To-Do List"
 }
+
+variable "api_gateway_name" {
+  description = "Lambda function name"
+  default     = "todo-list"
+}
+
+variable "api_gateway_description" {
+  description = "Lambda function description"
+  default     = "A To-Do List"
+}
